64860ee373c0e92acdde1489933c1cd7b2eab506
superpingu <arnaudbonetti@me.com>
2016-04-13 14:07:07 +0200

    added file save

--- robot/app.js
+++ robot/app.js
@@ -2,6 +2,7 @@ var name = require("/var/apps/rpc/config.json").name;
 var socket = require("socket.io-client")("http://abonetti.fr:3003/"+name);
 var fs = require("fs");
 
+var pathsDir = "/var/paths/";
 
 socket.on('connect', function(){
     console.log("Connected to the server");
@@ -19,8 +20,33 @@ socket.on('paths', function(data) {
         }
     }
 });
-
+function saveFile(name, content) {
+    fs.writeFile(name, content, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log(name+" saved !");
+    });
+}
+function dumpPoints(points) {
+    var str ='';
+    for (var i = 0; i < points.length; i++) {
+        str += ' ' + points[i].x + ' ' + points[i].y;
+    }
+    return str;
+}
+function mirrorPoints(points) {
+    var tab ='';
+    for (var i = 0; i < points.length; i++) {
+        tab.push({x: 3000-points[i].x, y:points[i].y});
+    }
+    return tab;
+}
 function savePath(name, path) {
+    var prefix = path.cruiseSpeed + ' ' + path.endSpeed;
     console.log("Saving path "+ name);
-    // TODO : save the paths in several files (according to seashells config and team)
+    for (var i = 0; i < 5; i++) {
+        saveFile(pathsDir+name+'-green-'+i+'.path', prefix+dumpPoints(path.green[i].points));
+        saveFile(pathsDir+name+'-purple-'+i+'.path', prefix+dumpPoints(mirrorPoints(path.purple[i].points)));
+    }
 }
